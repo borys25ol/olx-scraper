2cfa5237a27ab0e43a9b73903944aa5e84b7d94d
borys25ol <oleynik.boris@gmail.com>
2019-08-31 01:31:21 +0300

    handle response from olx_api

--- olx_scraper/olx_scraper/settings.py
+++ olx_scraper/olx_scraper/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'olx_scraper.spiders'
 #USER_AGENT = 'olx_scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- olx_scraper/olx_scraper/spiders/olx_pk.py
+++ olx_scraper/olx_scraper/spiders/olx_pk.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import json
+import re
+
 import scrapy
 from scrapy import Request
 
@@ -10,12 +13,31 @@ class OlxPkSpider(scrapy.Spider):
 
     start_urls = [
         'https://www.olx.com.pk/property-for-rent_c3',
-        'https://www.olx.com.pk/property-for-sale_c2'
     ]
 
     custom_settings = {
         'DUPEFILTER_CLASS': 'scrapy.dupefilters.BaseDupeFilter',
     }
+
+    products_on_page_re = re.compile('(.+) ads in')
+
+    def __init__(self):
+        super(OlxPkSpider, self).__init__()
+        self.olx_api_pattern = 'https://www.olx.com.pk/api/locations?parent={location}'
+        self.olx_page_pattern = 'https://www.olx.com.pk/{}{}'
+
+    @staticmethod
+    def get_data_from_json(response):
+        data = json.loads(response.text)
+        data = data.get('data')
+        return data
+
+    @staticmethod
+    def make_url(url_pattern, name, location_id, head):
+        name = name.lower().replace(' ', '-')
+        id_part = '_g' + str(location_id)
+        url = url_pattern.format(name, id_part) + head
+        return url
 
     def start_requests(self):
         for url in self.start_urls:
@@ -28,4 +50,39 @@ class OlxPkSpider(scrapy.Spider):
             yield response.follow(url=url, callback=self.parse_category)
 
     def parse_category(self, response):
-        pass
+        numbers = re.findall('\d+', response.url)
+        location_id = numbers[0]
+        category_id = numbers[-1]
+
+        products_count = response.css('p.FYmzo::text').re_first(self.products_on_page_re)
+        if products_count:
+            products_count = products_count.replace(',', '')
+            count = int(products_count)
+
+            if count and count > 2000:
+                if response.url.endswith('/'):
+                    response.meta['path'] = '/' + response.url.split('/')[-2]
+                else:
+                    response.meta['path'] = '/' + response.url.split('/')[-1]
+                if category_id == location_id:
+                    url = self.olx_api_pattern.format(location='1000001')
+                else:
+                    url = self.olx_api_pattern.format(location=location_id)
+
+                yield response.request.replace(
+                    url=url,
+                    callback=self.parse_olx_api,
+                    meta=response.meta
+                )
+
+    def parse_olx_api(self, response):
+        head_path = response.meta.get('path')
+        data = self.get_data_from_json(response)
+        if data:
+            id_list = [(item['id'], item['name']) for item in data]
+            for id, name in id_list:
+                url = self.make_url(self.olx_page_pattern, name, id, head_path)
+                yield response.request.replace(
+                    url=url,
+                    callback=self.parse_category
+                )
