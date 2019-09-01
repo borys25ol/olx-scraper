8d2088fb30da450d317b7194e372d4003e6ced97
borys25ol <oleynik.boris@gmail.com>
2019-09-01 20:01:33 +0300

    add pipelines from 'purpose' and 'image_url' fields

--- olx_scraper/olx_scraper/pipelines.py
+++ olx_scraper/olx_scraper/pipelines.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from olx_scraper.items import OlxItem
 
 
 class OlxScraperPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class InvalidPurposePipeline(object):
+
+    def __init__(self):
+        self.valid_purpose = ['rent', 'sale']
+
+    def process_item(self, item, spider):
+        if isinstance(item, OlxItem):
+            purpose = item.get('purpose')
+            if purpose:
+                if 'sale' in purpose.lower() or 'rent' in purpose.lower():
+                    return item
+        else:
+            return item
+
+
+class HandleStaticImageUrlPipeline(object):
+
+    def process_item(self, item, spider):
+        if isinstance(item, OlxItem):
+            images = item.get('image_urls')
+            if 'statics.olx.com.pk' in images:
+                item['image_urls'] = ''
+                item['count_of_images'] = 0
         return item
